bccf22f32d074618bd6e2d38e2c649cea6db8ed0
jocooper <jocooper2018@gmail.com>
2025-01-26 08:58:32 +0100

    Added 404 return when trying to update a task that does'nt exist.

--- backend/src/Repositories/TaskRepository.ts
+++ backend/src/Repositories/TaskRepository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../PrismaService';
 import { Prisma } from '@prisma/client';
 
@@ -27,6 +27,16 @@ export default class TaskRepository {
       return this.prisma.task.create({
         data: data as Prisma.TaskCreateInput,
       });
+    }
+
+    const taskExist = await this.prisma.task.findUnique({
+      where: {
+        id: (data as Prisma.TaskUncheckedUpdateInput).id as number
+      },
+    });
+
+    if (!taskExist) {
+      throw new NotFoundException(`Task with id ${(data as Prisma.TaskUncheckedUpdateInput).id} not found`);
     }
 
     return this.prisma.task.update({
